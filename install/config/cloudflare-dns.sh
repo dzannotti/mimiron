7f6f464276da109b24a38cde8be2882e3cb02e17
Daniele Zannotti <d.zannotti@me.com>
2025-11-03 11:47:05 +0000

    more starship changes

--- install/config/cloudflare-dns.sh
+++ install/config/cloudflare-dns.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-# Configure Cloudflare DNS globally for all NetworkManager connections
+# Configure Cloudflare DNS for active NetworkManager connections
 
 echo "Configuring Cloudflare DNS..."
 
-# Create NetworkManager config to use Cloudflare DNS
-sudo tee /etc/NetworkManager/conf.d/dns.conf > /dev/null << 'NMCONF'
-[global-dns]
-searches=
+# Get all active connection UUIDs
+ACTIVE_CONNECTIONS=$(nmcli -t -f UUID,TYPE connection show --active | grep -E 'ethernet|wifi|wireless' | cut -d: -f1)
 
-[global-dns-domain-*]
-servers=1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001
-NMCONF
+if [[ -z "$ACTIVE_CONNECTIONS" ]]; then
+  echo "No active network connections found, skipping DNS configuration"
+  return 0
+fi
 
-# Restart NetworkManager to apply changes
-sudo systemctl restart NetworkManager
+# Configure DNS for each active connection
+for UUID in $ACTIVE_CONNECTIONS; do
+  CONNECTION_NAME=$(nmcli -t -f NAME connection show "$UUID")
+  echo "Setting Cloudflare DNS for connection: $CONNECTION_NAME"
 
-echo "Cloudflare DNS configured for all interfaces"
+  # Set IPv4 DNS servers
+  nmcli connection modify "$UUID" ipv4.dns "1.1.1.1 1.0.0.1"
+  nmcli connection modify "$UUID" ipv4.ignore-auto-dns yes
+
+  # Set IPv6 DNS servers
+  nmcli connection modify "$UUID" ipv6.dns "2606:4700:4700::1111 2606:4700:4700::1001"
+  nmcli connection modify "$UUID" ipv6.ignore-auto-dns yes
+done
+
+# Reload connections to apply changes (doesn't restart NetworkManager)
+nmcli connection reload
+
+echo "Cloudflare DNS configured for active connections"
+echo "Note: You may need to reconnect to your network for changes to take effect"
